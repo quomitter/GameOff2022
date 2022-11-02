2e4df02c67a949394c28b1e8da99724288fab9bd
quomitter <109629602+quomitter@users.noreply.github.com>
2022-11-02 03:37:16 -0400

    Update EnemyController.cs

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private EnemyHealthBar enemyHealthBar; 
+
+    public int randomNumber; 
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyHealthBar = FindObjectOfType<EnemyHealthBar>(); 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        randomNumber = Random.Range(1, 4);
+
+        switch (randomNumber)
+        {
+            case 1:
+                //move toward player
+                break;
+            case 2:
+                //Kick
+                break;
+            case 3:
+                //Punch
+                break;
+        }
     }
 
 
