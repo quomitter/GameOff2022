20654cb0ed54dc5f92fd0c97da1dda8b183b96ad
quomitter <109629602+quomitter@users.noreply.github.com>
2022-11-06 02:14:53 -0500

    changes

---
+++ Assets/Timer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class Timer : MonoBehaviour
+{
+    public TMP_Text timer;
+    public float countDownTimer;
+    private PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+        countDownTimer = 99.0f;
+        timer.text = "99";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController.gameIsActive)
+        {
+            countDownTimer -= Time.deltaTime;
+            timer.text = System.Convert.ToString(Mathf.RoundToInt(countDownTimer));
+        }
+    }
+}
